e1fc80d9c3b00a45f42576a32c1a047ebfa43758
jhigger <njohnjhigger@gmail.com>
2024-02-17 21:22:39 +0800

    add section1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
+/* eslint-disable @next/next/no-img-element */
 import Head from "next/head";
+import Balancer from "react-wrap-balancer";
 import Header from "~/components/Header";
 
 export default function Home() {
@@ -11,7 +13,38 @@ export default function Home() {
       </Head>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-center bg-[#E4E2DD]">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
+        <img
+          src="https://placehold.co/1280x720/?text=Image"
+          alt="hero"
+          className="h-full w-full object-cover"
+        />
+        <section className="container flex flex-col items-center justify-center gap-12 py-60 ">
+          <h1 className="text-xl font-light uppercase tracking-widest">
+            Editorial Wedding Photographer
+          </h1>
+          <div className="grid grid-cols-1 place-items-center gap-8 md:grid-cols-3">
+            <img
+              src="https://placehold.co/800x1200/?text=Image"
+              alt="sample"
+              className="h-[80%] rounded-xl"
+            />
+            <img
+              src="https://placehold.co/800x1200/?text=Image"
+              alt="sample"
+              className="rounded-xl"
+            />
+            <img
+              src="https://placehold.co/800x1200/?text=Image"
+              alt="sample"
+              className="h-[80%] rounded-xl"
+            />
+          </div>
+          <span className="max-w-[700px] text-center text-5xl font-light uppercase leading-tight">
+            <Balancer>
+              Exquisite Photography <i>For</i> Modern Couples
+            </Balancer>
+          </span>
+        </section>
       </main>
     </>
   );
