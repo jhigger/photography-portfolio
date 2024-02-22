30e7b95bc7e2c369190717d740628046fb52a7c5
jhigger <njohnjhigger@gmail.com>
2024-02-22 15:49:09 +0800

    add section5

---
+++ src/components/Section5.tsx
@@ -0,0 +1,89 @@
+/* eslint-disable @next/next/no-img-element */
+
+const Section5 = () => {
+  return (
+    <section className="overflow-hidden">
+      <div className="container mx-auto grid grid-cols-5 place-items-center gap-10 pb-20 pt-44">
+        <div className="col-span-1 col-start-1 row-span-6 row-start-3 mr-60 h-[500px] w-[350px]">
+          <img
+            src="https://placehold.co/400x600/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+
+        <div className="col-span-1 col-start-2 row-span-5 row-start-2 h-[350px] w-[250px]">
+          <img
+            src="https://placehold.co/400x600/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+        <div className="col-span-1 col-start-3 row-span-2 row-start-1 h-[163px] w-[250px]">
+          <img
+            src="https://placehold.co/400x300/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+        <div className="col-span-1 col-start-4 row-span-5 row-start-2 h-[350px] w-[250px]">
+          <img
+            src="https://placehold.co/400x600/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+
+        <div className="col-span-1 col-start-3 row-span-3 row-start-3 flex w-[250px] flex-col items-center justify-center gap-4 text-center">
+          <span className="text-xs uppercase tracking-[0.25em]">
+            Recommended By
+          </span>
+          <img
+            src="https://placehold.co/400x100/?text=Image"
+            alt="sample"
+            className="h-[100px] w-[250px] rounded-xl object-cover"
+          />
+        </div>
+        <div className="col-span-1 col-start-3 row-span-2 row-start-6 w-[250px] text-center">
+          <img
+            src="https://placehold.co/400x100/?text=Image"
+            alt="sample"
+            className="h-[100px] w-[250px] rounded-xl object-cover"
+          />
+        </div>
+
+        <div className="col-span-1 col-start-2 row-span-3 row-start-7 h-[163px] w-[250px]">
+          <img
+            src="https://placehold.co/400x300/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+        <div className="col-span-1 col-start-3 row-span-5 row-start-8 h-[350px] w-[250px]">
+          <img
+            src="https://placehold.co/400x600/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+        <div className="col-span-1 col-start-4 row-span-3 row-start-7 h-[163px] w-[250px]">
+          <img
+            src="https://placehold.co/400x300/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+
+        <div className="col-span-1 col-start-5 row-span-6 row-start-3 ml-60 h-[500px] w-[350px]">
+          <img
+            src="https://placehold.co/400x600/?text=Image"
+            alt="sample"
+            className="h-full w-full rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Section5;
