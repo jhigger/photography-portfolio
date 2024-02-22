dd2398b68ae43137571e0b46aed8667782795421
jhigger <njohnjhigger@gmail.com>
2024-02-23 05:04:33 +0800

    add section8

---
+++ src/components/Section8.tsx
@@ -0,0 +1,62 @@
+/* eslint-disable @next/next/no-img-element */
+
+const Section8 = () => {
+  return (
+    <section className="bg-white py-40">
+      <div className="container mx-auto flex flex-col items-center justify-center gap-20">
+        <div className="-mt-60 text-9xl font-light uppercase text-gray-400">
+          Instagram
+        </div>
+        <div className="flex h-full w-full items-center justify-center gap-10">
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+          <div className="aspect-square">
+            <img
+              src="https://placehold.co/5000x500/?text=Image"
+              alt="sample"
+              className="size-full rounded-xl object-cover object-center"
+            />
+          </div>
+        </div>
+        <span className="text-sm font-light uppercase tracking-[0.4em] underline">
+          @Renai.Photo
+        </span>
+      </div>
+    </section>
+  );
+};
+
+export default Section8;
