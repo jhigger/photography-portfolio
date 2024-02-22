9cb0973132b2165e89e6759571a34c56a4d33389
jhigger <njohnjhigger@gmail.com>
2024-02-22 13:44:25 +0800

    add section4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Hero from "~/components/Hero";
 import Section1 from "~/components/Section1";
 import Section2 from "~/components/Section2";
 import Section3 from "~/components/Section3";
+import Section4 from "~/components/Section4";
 
 export default function Home() {
   return (
@@ -25,6 +26,7 @@ export default function Home() {
         <Section1 />
         <Section2 />
         <Section3 />
+        <Section4 />
       </main>
     </>
   );
